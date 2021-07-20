76b73797140ae1b57c062d3cd2bd18276c8a5802
Qian Yangyang 钱洋洋 <hfutqyy@163.com>
2021-07-20 18:01:48 +0800

    一些更新

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-import mindspore as ms
+import os, sys
+BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # __file__获取执行文件相对路径，整行为取上一级目录
+sys.path.append(BASE_DIR)
 
-print("Hello")
+
+import mindspore as ms
+from cv import test_cv
+
+
+test_cv.func()
+print(sys.path)
+
